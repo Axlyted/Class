7fde3c2aa50b6afee31a8e7db1e633d97f53f5c9
Axlyted <Axlyted@desktop>
2016-03-20 15:15:38 +0200

    testing

---
+++ src/myBot.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class myBot{
+  public static void main(String[] args)
+                             throws AWTException{
+    Robot Robot = new Robot();
+    for(int i  = 0; i<100; i++){
+    
+       Robot.mouseMove(500,500);
+    }
+    
+  }
+
+}
